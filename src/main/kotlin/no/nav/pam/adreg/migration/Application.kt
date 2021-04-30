c08b4e5927926508b93f926f91c2114e8de69d12
Øyvind Stegard <oyvind.stegard@nav.no>
2021-04-30 17:36:30 +0200

    Wait for cloud sql proxy before Spring init
    Experimental wait for connectable port to cloud sql proxy. Instead of letting
    Flyway fail hard during init.

--- src/main/kotlin/no/nav/pam/adreg/migration/Application.kt
+++ src/main/kotlin/no/nav/pam/adreg/migration/Application.kt
@@ -8,6 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
+import java.io.IOException
+import java.lang.RuntimeException
+import java.net.InetAddress
+import java.net.Socket
 import java.time.LocalDateTime
 import java.util.*
 
@@ -15,7 +19,29 @@ import java.util.*
 class Application
 
 fun main(args: Array<String>) {
+	// Delay startup until db port can be connected to (wait for cloud sql proxy)
+	val dbHost = System.getenv("PAMADREGDB_HOST")?:throw RuntimeException("Missing environment variable PAMADREGDB_HOST")
+	val dbPort = Integer.parseInt(System.getenv("PAMADREGDB_PORT"))
+
+	println("Waiting for database port at ${dbHost}:${dbPort} to become connectable ..")
+	waitForPort(dbHost, dbPort)
+
 	runApplication<Application>(*args)
+}
+
+fun waitForPort(host: String, port: Int): Boolean {
+	for (attempt in IntRange(1, 10)) {
+		try {
+			Socket(InetAddress.getByName(host), port).use {
+				println("Socket at ${host}:${port} is connectable at attempt ${attempt}")
+				return true
+			}
+		} catch (io: IOException) {
+			println("Socket at ${host}:${port} is not ready yet: ${io.message}")
+		}
+		Thread.sleep(1000)
+	}
+	return false
 }
 
 @Component
