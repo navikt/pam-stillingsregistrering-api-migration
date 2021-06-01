e6b7c561ad9fd474c5dbd54e794703ad461188ee
Øyvind Stegard <oyvind.stegard@nav.no>
2021-06-01 11:34:58 +0200

    s/PathParam/PathVariable

--- src/main/kotlin/no/nav/pam/adreg/migration/replication/AnnonseLookupController.kt
+++ src/main/kotlin/no/nav/pam/adreg/migration/replication/AnnonseLookupController.kt
@@ -4,16 +4,16 @@ import no.nav.pam.adreg.migration.annonse.AnnonseRepository
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import javax.websocket.server.PathParam
 
 @RestController
 @RequestMapping("/replication")
 class AnnonseLookupController(val annonseRepository: AnnonseRepository) {
 
     @GetMapping("/ad/{uuid}", produces = [MediaType.APPLICATION_JSON_VALUE])
-    fun adLookup(@PathParam("uuid") uuid: String): ResponseEntity<AnnonseReplicationDto> =
+    fun adLookup(@PathVariable("uuid") uuid: String): ResponseEntity<AnnonseReplicationDto> =
         annonseRepository.findByUuid(uuid)?.let { ResponseEntity.ok(fromAnnonse(it)) }?:ResponseEntity.notFound().build()
 
 }
